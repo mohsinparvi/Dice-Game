e7e0dfc1e41cdf5445d8f8777dd7463cdccae3bf
john emmi <johnemmi83@gmail.com>
2022-10-04 16:55:28 +0500

    modify the code.

--- script.js
+++ script.js
@@ -13,15 +13,24 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting Condition.
-score0El.textContent = 0;
-score1EL.textContent = 0;
-diceEl.classList.add('hidden');
+let score, currentScore, activePlayer, playing;
 
-//Dice rolling functionality
-
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const initialization = function () {
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1EL.textContent = 0;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+initialization();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent =
@@ -32,47 +41,55 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+//Dice rolling functionality
 btnRoll.addEventListener('click', function () {
-  //1- Generating new number;
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  //   const dice = Math.trunc(math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    //1- Generating new number;
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //   const dice = Math.trunc(math.random() * 6) + 1;
+    console.log(dice);
 
-  //2- Display Dice
+    //2- Display Dice
 
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3- check for roll 1: if true,
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // current0EL.textContent = currentScore;
-  } else {
-    // switched the player
-    // document.classList.add('player--acitve');
-    switchPlayer();
+    //3- check for roll 1: if true,
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0EL.textContent = currentScore;
+    } else {
+      // switched the player
+      // document.classList.add('player--acitve');
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //1- add current score to active player's score
-  score[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
-  //2. check if the player scores>=100 then finished the game
-  if (score[activePlayer] >= 10) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--active');
+  if (playing) {
+    //1- add current score to active player's score
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    //2. check if the player scores>=100 then finished the game
+    if (score[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
 
-    // document.querySelector(`.player--${activePlayer}`).textContent = 'winner';
-  } else {
-    switchPlayer();
+      // document.querySelector(`.player--${activePlayer}`).textContent = 'winner';
+    } else {
+      switchPlayer();
+    }
+    //3. switched the player
   }
-  //3. switched the player
 });
+btnNew.addEventListener('click', initialization);
